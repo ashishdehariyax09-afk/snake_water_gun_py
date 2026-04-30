4544361a4f57a2586eb76dfc11cf1e08fa8309ab
ashishdehariyax09-afk <ashishdehariyax09@gmail.com>
2026-05-01 01:38:19 +0530

    Add files via upload

---
+++ snake_water_gun_py.py
@@ -0,0 +1,31 @@
+#  '''
+# 1 for snake 
+# -1 for water 
+# 0 for gun
+#    '''
+import random
+computer= random.choice([0,-1,1])
+youstr =input("Enter the choice:")
+yourDict={"s":1,"w":-1,"g":0}
+reverseDict={1:"Snake",-1:"Water",0:"Gun"}
+you=yourDict[youstr]
+
+print(f"You choose{reverseDict[you]}\nComputer choose{reverseDict[computer]}")
+if(computer==you):
+    print("Its a draw")
+else:
+    if(computer==-1 and you==1):#0
+        print("You win!")
+    elif(computer==-1 and you==0):#-1
+        print("Computer win!")
+    elif(computer==0 and you==-1):#-1
+        print("You win!")
+    elif(computer==0 and you==1):#1
+        print("Computer win!")
+    elif(computer==1 and you==0):#1
+        print("Computer win!")
+    elif(computer==1 and you==-1):#0
+        print("Computer win!")
+    else:
+        print("Invalid Input")
+    
